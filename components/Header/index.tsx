95006153517d0393fe6d989f9b5f1772442b34ac
vakulanikita <nik.vakula@bk.ru>
2021-12-18 18:24:54 +0300

    turn logo to link

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -19,11 +19,11 @@ export const Header: React.FC = () => {
         <IconButton>
           <MenuIcon />
         </IconButton>
-        <svg className={styles.logo} viewBox="0 0 24 25">
-          <path fill="#e8a427" d="M0 19h8.5v6H0v-6z"></path>
-          <path d="M0 7h8.5v18l6.5-6V7h9V0H0v7z"></path>
-          <path fill="rgba(0,0,0,0.15)" d="M7.5 19h1v6l-1-6z"></path>
-        </svg>
+        <NextLink href="/">
+          <a>
+            <img height={35} className="mr-20" src="/static/img/logo.svg" alt="Logo" />
+          </a>
+        </NextLink>
 
         <div className={styles.searchBlock}>
           <SearchIcon />
