235c991f3b0c3f2715e49e81fdb3ad504c12f198
vakulanikita <nik.vakula@bk.ru>
2021-12-20 09:59:56 +0300

    add comments under posts

--- pages/news/[slug].tsx
+++ pages/news/[slug].tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { MainLayout } from '../../layouts/main-layout';
 import { FullPost } from '../../components/FullPost';
-// import { PostComments } from "../../components/PostComments";
+import { PostComments } from "../../components/PostComments";
 
 export default function Post() {
   return (
     <MainLayout className="mb-50" contentFullWidth>
       <FullPost />
-      {/* <PostComments /> */}
+      <PostComments />
     </MainLayout>
   );
 }
